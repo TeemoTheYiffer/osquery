6545ad810b576d5824f31c2c4f13ad427704582a
jaguirre3 <joe_aguirre@intuit.com>
2021-07-12 19:06:25 -0700

    Including Uninstall for Clean Install

--- src/osquery.py
+++ src/osquery.py
@@ -7,14 +7,19 @@
 # Description = Osquery deployment script
 ##*===============================================
 
-from utils import LogHandler, osquery_install, service_check, error_email
+from utils import LogHandler, osquery_install, service_check, email, osquery_uninstall
 import logging
 
 # Logging
 log_handle = LogHandler().logger # delete 'utils.' and unedit utils import line for packaging
 log = logging.LoggerAdapter(log_handle, {})
 
+# Uninstalls old installs to start fresh
+log.info(f"Uninstalling Osquery (If Needed)...")
+osquery_uninstall()
+
 # Downloads, Installs, Configures, & Validates osquery
+log.info(f"Installing Osquery...")
 osquery_install("4.9.0")
 
 # Launchctl Osqueryd Service Smoke Test
@@ -22,4 +27,7 @@ if not service_check("com.datadog.osqueryd"):
     log.error("Osqueryd service was not found! Sending e-mail notification.")
     import getpass
     password=getpass.getpass("Enter your e-mail password\n") # Only for testing; Leverage a secrets vault (ie AWS SecretsManager)
-    error_email(sender="joeaguirre0@gmail.com", receiver="joe_aguirre@intuit.com", password=password, smtp_server="smtp.gmail.com", port=465, subject="ERROR: osqueryd service was not found!")
+    email(sender="joeaguirre0@gmail.com", receiver="joe_aguirre@intuit.com", password=password, smtp_server="smtp.gmail.com", port=465, subject="ERROR: osqueryd service was not found!")
+
+# Deploys Intrusion Detection app
+# SOON #
